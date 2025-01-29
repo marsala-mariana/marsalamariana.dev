1ffdb807b244c2e8e0af4114b9019f76ede4cbb5
Mariana Marsala <stephaniemarsala08gmail.com>
2025-01-29 19:39:18 -0300

    fotos details

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -1,34 +1,42 @@
 import { Box, IconButton, Typography } from "@mui/material";
 import image from "../assets/bodyProject.png";
 import {IconArrowLeft, IconBrandAdobeIllustrator, IconBrandFigma, IconBrandWordpress, IconFileTypeCss } from "@tabler/icons-react";
-//import imageRouss from "../assets/rouss.png"
 import { useNavigate, useParams } from "react-router-dom";
 import { projectsData } from "../utils/projectsData";
+import { useEffect, useState } from "react";
 
 
 export const ProjectDetail = () => {
   const navigate = useNavigate();
-   const { id } = useParams<{ id: string }>(); // Obtiene el ID desde la URL
-   const project = projectsData.find((p) => p.id === id); // Busca el proyecto en los datos
+  const { id } = useParams<{ id: string }>();
+  const project = projectsData.find((p) => p.id === id);
 
-   if (!project) {
-     return <p>Proyecto no encontrado</p>;
-   }
+   const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    // Verificar si coverImage está definido y tiene longitud
+    const coverImage = project?.coverImage;
+    if (Array.isArray(coverImage) && coverImage.length > 0) {
+      const interval = setInterval(() => {
+        setImageIndex((prevIndex) => (prevIndex + 1) % coverImage.length);
+      }, 3000); // Cambia la imagen cada 3 segundos
+
+      return () => clearInterval(interval); // Limpiar el intervalo cuando el componente se desmonte
+    }
+  }, [project?.coverImage]);
 
   
   return (
     <Box
       id="details"
       sx={{
-        height: "100vh",
         backgroundImage: `url(${image})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         position: "relative",
-        paddingBottom: 20,
+        paddingBottom: 6,
       }}
     >
-      {/* Botón de regreso */}
       <IconButton
         onClick={() => navigate(-1)}
         sx={{
@@ -44,7 +52,7 @@ export const ProjectDetail = () => {
       </IconButton>
       <Box
         width="100%"
-        sx={{ display: "flex", paddingTop: 20, justifyContent: "center" }}
+        sx={{ display: "flex", paddingTop: 15, justifyContent: "center" }}
       >
         <Box
           width="58%"
@@ -57,38 +65,39 @@ export const ProjectDetail = () => {
               textAlign: "center",
             }}
           >
-            {project.nameProyect} - {project.plataforma}
+            {project?.nameProyect} - {project?.plataforma}
           </Typography>
 
           {/* Contenedores de tecnologías */}
-          <Box display="flex" gap={3} mb={4} flexWrap="wrap">
-            {project.technologies.map((tech, index) => {
-              const isUXUI =
-                tech.toLowerCase().includes("ux") ||
-                tech.toLowerCase().includes("ui");
+          <Box display="flex" gap={3} mb={2} flexWrap="wrap">
+            {project?.etiqueta &&
+              project.etiqueta.map((tech, index) => {
+                const isUXUI =
+                  tech.toLowerCase().includes("ux") ||
+                  tech.toLowerCase().includes("ui");
 
-              return (
-                <Box
-                  key={index}
-                  sx={{
-                    border: "2px solid",
-                    borderColor: isUXUI ? "#FF1493" : "#A962FF",
-                    backgroundColor: "#111111",
-                    padding: "5px 16px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    borderRadius: "6px",
-                  }}
-                >
-                  <Typography
-                    sx={{ fontSize: 10, fontWeight: 500, color: "white" }}
+                return (
+                  <Box
+                    key={index}
+                    sx={{
+                      border: "2px solid",
+                      borderColor: isUXUI ? "#FF1493" : "#A962FF",
+                      backgroundColor: "#111111",
+                      padding: "5px 16px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      borderRadius: "6px",
+                    }}
                   >
-                    {tech}
-                  </Typography>
-                </Box>
-              );
-            })}
+                    <Typography
+                      sx={{ fontSize: 10, fontWeight: 500, color: "white" }}
+                    >
+                      {tech}
+                    </Typography>
+                  </Box>
+                );
+              })}
           </Box>
         </Box>
       </Box>
@@ -109,23 +118,52 @@ export const ProjectDetail = () => {
           justifyContent="center"
           width="58%"
         >
-          {/* Imagen de fondo gris */}
           <Box
             sx={{
               width: { xs: "100%", md: "50%" },
-              height: 290,
-              backgroundImage: `url(${project.coverImage})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              height: 250,
               borderRadius: "18px 0px 0px 18px",
+              position: "relative",
+              overflow: "hidden",
             }}
-          />
+          >
+            {Array.isArray(project?.coverImage) &&
+            project.coverImage.length > 0 ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  height: "100%",
+                  width: "100%",
+                  transition: "transform 0.5s ease-in-out",
+                  transform: `translateX(-${imageIndex * 100}%)`,
+                  backgroundImage: `url(${project.coverImage[imageIndex]})`,
+                 
+                  backgroundSize: "contain",
+                  backgroundPosition: "center",
+                }}
+              />
+            ) : (
+              <img
+                src={
+                  Array.isArray(project?.coverImage)
+                    ? project.coverImage[0]
+                    : project?.coverImage
+                }
+                alt={project?.nameProyect}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderRadius: "18px 0px 0px 18px",
+                }}
+              />
+            )}
+          </Box>
 
-          {/* Caja de información */}
           <Box
             sx={{
               width: { xs: "100%", md: "50%" },
-              height: 290,
+              height: 250,
               backgroundColor: "#1A1A1A",
               borderRadius: "0px 18px 18px 0px",
               padding: 3,
@@ -157,17 +195,18 @@ export const ProjectDetail = () => {
                   sx={{
                     color: "white",
                     mt: 2,
-                    fontSize: 12,
+                    fontSize: 10,
                     textAlign: "justify",
                     fontWeight: 100,
+                    whiteSpace: "pre-line",
                   }}
                 >
-                  {project.description}
+                  {project?.description}
                 </Typography>
               </Box>
             </Box>
 
-            {project.enlace && (
+            {project?.enlace && (
               <a
                 href={project.enlace}
                 target="_blank"
@@ -181,7 +220,7 @@ export const ProjectDetail = () => {
                   textDecoration: "none",
                 }}
               >
-               Enlace
+                Enlace
               </a>
             )}
           </Box>
@@ -203,20 +242,24 @@ export const ProjectDetail = () => {
           sx={{
             display: "flex",
             gap: 2,
-            padding: 2,
+            padding: 1,
             borderRadius: 2,
             justifyContent: "space-around",
             width: "58%",
-            height: "55px",
+            height: "40px",
             backgroundColor: "#261F264D",
           }}
         >
-          <IconBrandFigma size={28} color="#7a00a3" />
+          <IconBrandFigma size={20} color="white" stroke-width="1" />
 
-          <IconBrandAdobeIllustrator size={28} color="#FF7C00" />
+          <IconBrandAdobeIllustrator
+            size={20}
+            color="#FF7C00"
+            stroke-width="1"
+          />
 
-          <IconFileTypeCss size={28} color="#FFFFFF" />
-          <IconBrandWordpress size={28} color="#21759B" />
+          <IconFileTypeCss size={20} color="#FFFFFF" stroke-width="1" />
+          <IconBrandWordpress size={20} color="#21759B" stroke-width="1" />
         </Box>
       </Box>
     </Box>
--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -1,41 +1,50 @@
-import imageRouss from "../assets/rouss.png"
-import nuvah from "../assets/nuvah.jpg";
+import roussFoto from "../assets/rouss.png";
+
+import nuvahsing from "../assets/mokupNuvah.png";
+import logoNu from "../assets/logo-nu.jpg";
+import etiqueta from "../assets/etiquetaNuvah.jpg";
+
 import logoAdv from "../assets/logoAd.png";
+import vasos from"../assets/vasos.png";
+import identiAdv from "../assets/identidadAdv.jpg"
+import cartelADV from "../assets/carteladv.png";
+
+import movilAppADV from "../assets/MobileADVAPP-01.png"
+import appRE from "../assets/iphone15.png"
+import mokups from "../assets/15ockup.jpeg"
 
 export const projectsData = [
   {
     id: "1",
     nameProyect: "Rouss",
     plataforma: "Website",
-    coverImage:  imageRouss ,
-    technologies: [
-      "Design UX - UI",
-      "WordPress",
-      "Figma",
-      "Adobe Illustraitor",
-      "CSS",
-    ],
+    coverImage: roussFoto,
+    technologies: ["WordPress", "Figma", "Adobe Illustraitor", "CSS"],
+    etiqueta: ["Design UX - UI"],
     description:
-      "Comencé el proyecto realizando una investigación exhaustiva sobre la marca 'Rouss Estética', analizando su identidad y público objetivo. Con base en esto, diseñé la interfaz de usuario (UI), creando un diseño moderno y alineado con la estética y valores de la marca. También trabajé en la creación del logotipo de 'Rouss Estética', asegurándome de que reflejara su enfoque profesional y elegante. Posteriormente, implementé el diseño utilizando WordPress, aprovechando sus herramientas y funcionalidades para optimizar la experiencia del usuario. Durante el desarrollo, me aseguré de mejorar la navegación y la estética del sitio, garantizando una interfaz amigable, funcional y visualmente atractiva.",
+      "Comencé el proyecto realizando una investigación exhaustiva sobre la marca 'Rouss Estética', analizando su identidad y público objetivo.\n\nCon base en esto, diseñé la interfaz de usuario (UI), creando un diseño moderno y alineado con la estética y valores de la marca. También trabajé en la creación del logotipo de 'Rouss Estética', asegurándome de que reflejara su enfoque profesional y elegante.\n\nPosteriormente, implementé el diseño utilizando WordPress, aprovechando sus herramientas y funcionalidades para optimizar la experiencia del usuario.\n\nDurante el desarrollo, me aseguré de mejorar la navegación y la estética del sitio, garantizando una interfaz amigable, funcional y visualmente atractiva.",
+
     enlace: "http://roussestetica.com/",
   },
   {
     id: "2",
     nameProyect: "Nuvah",
     plataforma: "Logo",
-    coverImage:  nuvah ,
+    coverImage: [nuvahsing, etiqueta, logoNu],
     technologies: ["Adobe Illustraitor"],
+    etiqueta: ["Design UX - UI"],
     description:
-      "Para este logo, realicé una investigación y entrevista con la marca para entender sus objetivos y lo que quería transmitir. Con esa información, diseñé un logo simple y moderno en Adobe Illustrator, utilizando colores frescos que reflejan la esencia de la marca y su enfoque juvenil y dinámico",
+      "Para este logo, realicé una investigación y entrevista con la marca para entender sus objetivos y lo que quería transmitir.\n\nCon esa información, diseñé un logo simple y moderno en Adobe Illustrator, utilizando colores frescos que reflejan la esencia de la marca y su enfoque juvenil y dinámico",
   },
   {
     id: "3",
     nameProyect: "ADV",
     plataforma: "Logo",
-    coverImage: logoAdv,
+    coverImage: [logoAdv, vasos, identiAdv, cartelADV],
     technologies: ["Adobe Illustraitor"],
+    etiqueta: ["Design UX - UI"],
     description:
-      "Para este logo, realicé una investigación y entrevista con la marca para entender sus objetivos y lo que quería transmitir. Con esa información, diseñé un logo simple y moderno en Adobe Illustrator, utilizando colores frescos que reflejan la esencia de la marca y su enfoque juvenil y dinámico",
+      "Para este logo, realicé una investigación y entrevista con la marca para entender sus objetivos y lo que quería transmitir.\n\nCon esa información, diseñé un logo simple y moderno en Adobe Illustrator, utilizando colores frescos que reflejan la esencia de la marca y su enfoque juvenil y dinámico",
   },
   {
     id: "4",
@@ -50,6 +59,7 @@ export const projectsData = [
       "Meterial-UI",
       "CI/CD con GitHub Actions",
     ],
+    etiqueta: ["Design UX - UI", "Developer Front-end"],
     description:
       "Lidero el diseño, desarrollo y ejecución de un sistema de gestión de comercios utilizando Next.js, TypeScript y Material-UI. Desarrollo el código base, implementando funcionalidades clave y asegurando un rendimiento eficiente y escalable. Realizo investigación de usuarios (UX Research) para identificar necesidades y transformar insights en interfaces intuitivas y funcionales. Diseño interfaces responsivas y accesibles que optimizan la experiencia del usuario. Implemento flujos de trabajo automatizados mediante pipelines de CI/CD con GitHub Actions, mejorando la eficiencia del desarrollo.",
     enlace: "",
@@ -57,6 +67,7 @@ export const projectsData = [
   {
     id: "5",
     nameProyect: "ADV",
+    coverImages: movilAppADV,
     plataforma: "Mobile App and Website",
     technologies: [
       "Adobe Illustraitor",
@@ -69,6 +80,7 @@ export const projectsData = [
       "Trello,",
       "GitHub,",
     ],
+    etiqueta: ["Design", "Developer Front-end"],
     description:
       "Diseñé y desarrollé una plataforma web con Next.js, TypeScript y Material-UI. Migré el proyecto a una aplicación móvil multiplataforma utilizando React Native y Expo, compatible con web, Android e iOS. Implementé la API de Google Maps Places y funciones de geolocalización para personalizar la experiencia del usuario. Diseñé interfaces consistentes y responsivas para las tres plataformas. Coordiné el equipo de desarrollo utilizando Scrum, Trello, y GitHub.",
     enlace: "https://www.adondevamos.club/search/suggestions",
@@ -76,8 +88,10 @@ export const projectsData = [
   {
     id: "6",
     nameProyect: "Design Rouss",
+    coverImages:[mokups,appRE] ,
     plataforma: "Design App RoussEstetica",
-    technologies: ["Adobe Illustraitor", "Figma", "Design UX - UI"],
+    technologies: ["Adobe Illustraitor", "Figma"],
+    etiqueta: ["Design UX-UI", "Developer "],
     description:
       "Diseño UX/UI para una aplicación. Llevé a cabo un proceso completo de UX Research, aplicando diversas metodologías como brief, benchmark, user personas, POV, encuestas y entrevistas para comprender las necesidades del usuario. A partir de estos hallazgos, definí el MVP, la arquitectura de información y realicé ejercicios de card sorting, task flow y flowchart para estructurar la navegación. Posteriormente, comencé a materializar la propuesta, desarrollando bocetos, wireframes en media y alta fidelidad, hasta llegar a la creación de un prototipo testeable, asegurando una experiencia de usuario óptima antes de su implementación.",
     enlace: "",
