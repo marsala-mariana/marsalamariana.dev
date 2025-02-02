39337cd662db217bd9d4a5f406b6ceb2141b20ee
Mimi Marsala <stephaniemarsala08@gmail.com>
2025-02-02 18:25:18 -0300

    porcentaje

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -120,7 +120,7 @@ export const Testimonials = () => {
               sx={{
                 textAlign: "center",
                 marginTop: 2,
-                width: { xs: "72%", sm: "48%", md: '60px' },
+                width: { xs: "72%", sm: "48%", md: '60%' },
               }}
             >
               <Typography
